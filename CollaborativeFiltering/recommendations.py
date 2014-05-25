fcf96b643c4a271b03237e51cb77adc6ca782865
vencejo <apussapus@gmail.com>
2014-05-25 13:14:19 +0200

    Introducidas las funciones topMatches y getRecommentadtions

--- CollaborativeFiltering/recommendations.py
+++ CollaborativeFiltering/recommendations.py
@@ -68,3 +68,43 @@ def sim_pearson(prefs, p1, p2):
 
 	return r
 
+# Returns the best matches for person from the prefs dictionary
+# Number of results and similarity function are optional params.
+def topMatches(prefs, person, n=5, similarity=sim_pearson):
+	scores = [(similarity(prefs,person,other), other) 
+						for other in prefs if other != person]
+	scores.sort()
+	scores.reverse()
+	return scores[0:n]
+
+# Gets recommendations for a person by using a weighted average
+# of every other user's rankings
+def getRecommendations(prefs,person,similarity=sim_pearson):
+	totals={}
+	simSums={}
+
+	for other in prefs:
+		# don't compare me to myself
+		if other == person: continue
+		sim=similarity(prefs,person,other)
+
+		#ignore scores of zero or lower
+		if sim<0: continue
+
+		for item in prefs[other]:
+			#only scores movies I haven't seen yet
+			if item not in prefs[person] or prefs[person][item]==0:
+				# Similarity * Score
+				totals.setdefault(item,0)
+				totals[item] += prefs[other][item]*sim
+				# Sum of similarities
+				simSums.setdefault(item,0)
+				simSums[item]+=sim
+
+	# Create the normalizated list
+	rankings=[(total/simSums[item], item) for item, total in totals.items()]
+
+	# Return the sorted list
+	rankings.sort()
+	rankings.reverse()
+	return rankings
