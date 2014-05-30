e15eef919f848005bc2352ce3ffbf27e625cb331
vencejo <apussapus@gmail.com>
2014-05-30 18:58:46 +0200

    Drawing the dendrogram

--- Discovering Groups/clusters.py
+++ Discovering Groups/clusters.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from PIL import Image, ImageDraw
 
 def pearson(v1,v2):
 	# simple sums
@@ -43,7 +44,7 @@ class bicluster	:
 	case, a blog). Each cluster also contains data about its location, which is either the
 	row data for the endpoints or the merged data from its two branches for other node
 	types. You can create a class called bicluster that has all of these properties, which
-	you’ll use to represent the hierarchical tree """
+	youll use to represent the hierarchical tree """
 
 	def __init__(self, vec,left=None,right=None,distance=0.0,id=None):
 		self.left = left
@@ -58,7 +59,7 @@ def hcluster(rows,distance=pearson):
 		matches by trying every possible pair and calculating their correlation. The best pair
 		of clusters is merged into a single cluster. The data for this new cluster is the average
 		of the data for the two old clusters. This process is repeated until only one cluster
-		remains. It can be very time consuming to do all these calculations, so it’s a good
+		remains. It can be very time consuming to do all these calculations, so its a good
 		idea to store the correlation results for each pair, since they will have to be calcu-
 		lated again and again until one of the items in the pair is merged into another cluster.
 	"""
@@ -85,7 +86,7 @@ def hcluster(rows,distance=pearson):
 					closest=d
 					lowestpair=(i,j)
 
-		# Calculate the average of the tow clusters
+		# Calculate the average of the two clusters
 		mergevec=[ (clust[lowestpair[0]].vec[i] + clust[lowestpair[1]].vec[i])/2.0 for i in range(len(clust[0].vec))]
 
 		# Create the new cluster
@@ -101,3 +102,83 @@ def hcluster(rows,distance=pearson):
 		clust.append(newcluster)
 
 	return clust[0]
+
+def printclust(clust, labels=None, n=0):
+	# Indent to make a hierarchy layout
+	for i in range(n): print " ",
+	if clust.id<0:
+		# negative id means that this is branch
+		print "-"
+	else:
+		# positive id means that this is branch
+		if labels == None: 
+			print clust.id
+		else:
+			print labels[clust.id]
+
+	# now print the right and left branches
+	if clust.left != None:
+		printclust(clust.left, labels=labels,n=n+1)
+	if clust.right != None:
+		printclust(clust.right, labels=labels, n=n+1)
+
+
+def getheight(clust):
+  # Is this an endpoint? Then the height is just 1
+  if clust.left==None and clust.right==None: return 1
+
+  # Otherwise the height is the same of the heights of
+  # each branch
+  return getheight(clust.left)+getheight(clust.right)
+
+def getdepth(clust):
+  # The distance of an endpoint is 0.0
+  if clust.left==None and clust.right==None: return 0
+
+  # The distance of a branch is the greater of its two sides
+  # plus its own distance
+  return max(getdepth(clust.left),getdepth(clust.right))+clust.distance
+
+
+def drawdendrogram(clust,labels,jpeg='clusters.jpg'):
+  # height and width
+  h=getheight(clust)*20
+  w=1200
+  depth=getdepth(clust)
+
+  # width is fixed, so scale distances accordingly
+  scaling=float(w-150)/depth
+
+  # Create a new image with a white background
+  img=Image.new('RGB',(w,h),(255,255,255))
+  draw=ImageDraw.Draw(img)
+
+  draw.line((0,h/2,10,h/2),fill=(255,0,0))    
+
+  # Draw the first node
+  drawnode(draw,clust,10,(h/2),scaling,labels)
+  img.save(jpeg,'JPEG')
+
+def drawnode(draw,clust,x,y,scaling,labels):
+  if clust.id<0:
+    h1=getheight(clust.left)*20
+    h2=getheight(clust.right)*20
+    top=y-(h1+h2)/2
+    bottom=y+(h1+h2)/2
+    # Line length
+    ll=clust.distance*scaling
+    # Vertical line from this cluster to children    
+    draw.line((x,top+h1/2,x,bottom-h2/2),fill=(255,0,0))    
+    
+    # Horizontal line to left item
+    draw.line((x,top+h1/2,x+ll,top+h1/2),fill=(255,0,0))    
+
+    # Horizontal line to right item
+    draw.line((x,bottom-h2/2,x+ll,bottom-h2/2),fill=(255,0,0))        
+
+    # Call the function to draw the left and right nodes    
+    drawnode(draw,clust.left,x+ll,top+h1/2,scaling,labels)
+    drawnode(draw,clust.right,x+ll,bottom-h2/2,scaling,labels)
+  else:   
+    # If this is an endpoint, draw the item label
+    draw.text((x+5,y-7),labels[clust.id],(0,0,0))
